ee239b033b6308f84109f9b81bc34a6dcf2a419b
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-05 16:49:41 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    
+    answer += hp / 5; hp -= (hp / 5) * 5;
+    answer += hp / 3; hp -= (hp / 3) * 3;
+    answer += hp / 1; 
+    return answer;
+}
