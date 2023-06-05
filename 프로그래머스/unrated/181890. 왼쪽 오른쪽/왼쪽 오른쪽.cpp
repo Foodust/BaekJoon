cc01e2156c59303cbecf9f5afe2093cd8147d20f
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-05 16:19:52 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> str_list) {
+    for (int i = 0; i < str_list.size(); i++)
+    {
+        if (str_list[i] == "l")
+            return vector<string>(str_list.begin(), str_list.begin() + i);
+        else if (str_list[i] == "r")
+            return vector<string>(str_list.begin() + i+1, str_list.end());
+    }
+    return vector<string>();
+}
