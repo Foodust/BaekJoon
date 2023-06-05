5c793189c237ddf93cd5b40867ca2f735196bbd7
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-05 16:05:44 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.cpp
+++ 프로그래머스/lv1/12901. 2016년/2016년.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 string solution(int a, int b) {
+    
     string answer = "";
     string day[7] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
     int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
