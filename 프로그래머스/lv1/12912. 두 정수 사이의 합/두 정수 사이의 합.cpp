a50fbd7bdc7d042558b10daeb03497dc47966f70
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-05 15:24:58 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 3.97 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer=0;
+    if(a < b)
+    {
+        for(int i = a; i<= b; ++i )
+        {
+            answer += i;
+        }
+    }
+    else if(a > b)
+    {
+
+        for(int i = b; i<= a; ++i )
+        {
+            answer += i;
+        }
+    }
+    else
+        answer =a;
+    return answer;
+}
