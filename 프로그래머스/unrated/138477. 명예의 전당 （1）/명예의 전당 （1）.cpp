7886ec396e9fe107600e6b1e0262acfa1184037d
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-05 16:04:34 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -8,6 +8,7 @@ vector<int> solution(int k, vector<int> score) {
     vector<int> asd;
     for (int i = 0; i < score.size(); i++)
     {
+        
         asd.push_back(score[i]);
         sort(asd.begin(), asd.end(), greater<int>());
         if (i < k)
