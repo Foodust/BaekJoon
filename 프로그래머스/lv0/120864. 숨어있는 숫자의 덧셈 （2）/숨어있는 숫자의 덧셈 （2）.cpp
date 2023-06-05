e21e942970d66de96b56a7195b7363475ba4645e
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-05 16:32:35 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    int n = 0;
+
+    for (int i = 0; i < my_string.size(); i++) 
+    {
+        if (isdigit(my_string[i]))
+        {
+            n = n * 10 + my_string[i] - '0';
+        }
+        else 
+        {
+            answer += n;
+            n = 0;
+        }
+    }
+    answer += n;
+    return answer;
+}
