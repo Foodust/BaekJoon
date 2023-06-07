1f52e6e5a6531e92ecc95d147168ec4cbb41b2c1
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-07 16:32:18 +0900

    [level 2] Title: 오픈채팅방, Time: 187.77 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    map<string, string> user;
+    vector<pair<string,int>> asd;
+    for (auto a : record)
+    {
+        stringstream stream;
+        string doing, id, name;
+        stream.str(a);
+        stream >> doing >> id >> name;
+        if (doing == "Enter")
+        {
+            user[id] = name;
+            asd.push_back({ id,1 });
+        }
+        else if (doing == "Leave")
+        {
+            asd.push_back({ id,0 });
+        }
+        else if (doing == "Change")
+        {
+            user[id] = name;
+        }
+    }
+    for (auto a : asd)
+    {
+        if (a.second == 0)
+            answer.push_back(user[a.first] + "님이 나갔습니다.");
+        else
+            answer.push_back(user[a.first] + "님이 들어왔습니다.");
+    }
+
+    return answer;
+}
