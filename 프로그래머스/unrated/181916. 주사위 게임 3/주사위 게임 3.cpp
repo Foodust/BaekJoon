6bb7490082f1adb04cddd15ed3b8e70ceabc5dc2
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-08 10:52:38 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.cpp
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.cpp
@@ -5,30 +5,6 @@
 using namespace std;
 
 int solution(int a, int b, int c, int d) {
-    return (
-        //1번째 
-        a == b && a == c && a == d ? 1111 * a :
-        //2번째
-        (a == b && a == c) ? pow(10 * a + d, 2) :
-        (a == b && a == d) ? pow(10 * a + c, 2) :
-        (a == c && a == d) ? pow(10 * a + b, 2) :
-        (b == c && b == d) ? pow(10 * b + a, 2) :
-        (c == b && c == d) ? pow(10 * c + a, 2) :
-        //5번째
-        a != b && a != c && a != d &&
-        b != c && b != d && c != d ? min({ a, b, c, d }) :
-        //4번째
-        a == b && b != c && c != d ? c * d :
-        a == c && a != b && b != d ? b * d :
-        a == d && b != c && c != d ? b * c :
-        b == c && a != d && c != d ? a * d :
-        b == d && a != c && a != d ? a * c :
-        c == d && a != b && c != a ? a * b :
-        //3번째
-        a == b && c == d ? (a + c) * (abs(a - c)) :
-        a == c && b == d ? (a + b) * (abs(a - b)) :
-        a == d && b == c ? (a + b) * (abs(a - b)) :
-
-        0
+    return (a == b && a == c && a == d ? 1111 * a : (a == b && a == c) ? pow(10 * a + d, 2) : (a == b && a == d) ? pow(10 * a + c, 2) :(a == c && a == d) ? pow(10 * a + b, 2) : (b == c && b == d) ? pow(10 * b + a, 2) : (c == b && c == d) ? pow(10 * c + a, 2) : a != b && a != c && a != d && b != c && b != d && c != d ? min({ a, b, c, d }) : a == b && b != c && c != d ? c * d : a == c && a != b && b != d ? b * d : a == d && b != c && c != d ? b * c : b == c && a != d && c != d ? a * d : b == d && a != c && a != d ? a * c : c == d && a != b && c != a ? a * b : a == b && c == d ? (a + c) * (abs(a - c)) : a == c && b == d ? (a + b) * (abs(a - b)) : a == d && b == c ? (a + b) * (abs(a - b)) : 0
         );
 }
