def6a80b9bbf06a721f74d97fe524c870899dc15
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-09 11:17:55 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+bool Check(string str)
+{
+    int len = (int)str.length();
+    stack<char> st;
+    for (int i = 0; i < len; i++)
+    {
+        char c = str[i];
+        if (c == '(')
+        {
+            st.push(str[i]);
+        }
+        else
+        {
+            if (!st.empty())
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+int main(void) 
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string str;
+        cin >> str;
+        if (Check(str))
+        {
+            cout << "YES" << endl;
+        }
+        else 
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
