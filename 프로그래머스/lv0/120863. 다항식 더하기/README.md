bdc2f626fb7633d986d9b3b09cb0ddc186fd5367
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-27 15:45:22 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/README.md
+++ 프로그래머스/lv0/120863. 다항식 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다항식 더하기 - 120863 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.02 ms
+메모리: 4.21 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.cpp
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <map>
@@ -16,25 +15,19 @@ string solution(string polynomial) {
         {
 			count = i;
 			if (temp.find('x') == string::npos)
-			{
                 m["y"] += stoi(temp);
-			}
             else
             {
                 if (temp.size() == 1)
                     m["x"]++;
                 else
-                {
                     m["x"] += stoi(temp.substr(0,count-1));
-                }
             }
 			temp = "";
             continue;
         }
         if (polynomial[i] != ' ' && polynomial[i] != '+')
-        {
             temp += polynomial[i];
-        }
     }
     for (auto iter : m)
     {
@@ -43,21 +36,11 @@ string solution(string polynomial) {
             answer = to_string(iter.second) + "x";
             if (answer == "1x")
                 answer = "x";
-
             if (m.size() >= 2)
-            {
                 answer += " + ";
-            }
         }
         else
-        {
             answer += to_string(iter.second);
-        }
     }
     return answer;
-}
-
-int main()
-{
-    cout << solution("3x + 7 + x");
 }
