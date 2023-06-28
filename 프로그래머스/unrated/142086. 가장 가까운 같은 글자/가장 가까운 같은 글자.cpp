4c058f5c12385de30e9648f7b8b13890fbe5a415
seunhoo <77597986+seunhoo@users.noreply.github.com>
2023-06-28 15:08:48 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.54 ms, Memory: 5.25 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+vector<int> solution(string s) {
+    vector<int> answer;
+    map<char, int> m;
+    for(int i = 0 ; i < s.size(); i++)
+    {
+        if (m.find(s[i]) == m.end())
+            answer.push_back(-1);
+        else
+            answer.push_back(i - m[s[i]]);
+        m[s[i]] = i;
+    }
+
+    return answer;
+}
