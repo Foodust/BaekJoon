18d546866da9814116bfa75b569e88f69a0f4f54
Foodust <77597986+Foodust@users.noreply.github.com>
2024-09-13 13:33:39 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,11 @@
+SELECT 
+    animal_id,
+    name,
+    CASE 
+        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+        ELSE 'X'
+    END AS 중성화
+FROM 
+    ANIMAL_INS
+ORDER BY 
+    ANIMAL_ID
